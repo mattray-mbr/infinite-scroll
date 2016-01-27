5c0c3b70cf3581deca3fdb37a385400f795f5ac1
Student <mattray.mbr@gmail.com>
2016-01-27 14:34:25 -0700

    infinite scroll is working

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 var app = angular.module('scroll', ['infinite-scroll'])
-	app.module = ('scrollController', ['$scope', function($scope){
+	app.controller('scrollController', ['$scope', function($scope){
 
 
+	$scope.images = [];
+	$scope.counter = 0;
+	$scope.loadMore = function(){
+		var last = $scope.images[$scope.images.length ];
+    	for(var i = 0; i <3; i++) {
+      		$scope.images.push(++$scope.counter);
+   		}
+	}
+	$scope.loadMore();
 	}]);
+
+
+// var myApp = angular.module('myApp', ['infinite-scroll']);
+
+// var numbersController = function($scope){
+//     $scope.numbers = [];
+//     $scope.counter = 0;
+
+//     $scope.loadMore = function () {
+//         for (var i = 0; i < 25; i++) {
+//             $scope.numbers.push(++$scope.counter);
+//         };
+//     }
+//     $scope.loadMore();
+// }
+// myApp.controller('numbersController', numbersController); 
