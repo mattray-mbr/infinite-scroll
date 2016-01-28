9f3ea6ff86fb45ee9986dbfb6aba42a30baaab07
Student <mattray.mbr@gmail.com>
2016-01-27 19:31:03 -0700

    added dates and start of input

--- app.js
+++ app.js
@@ -1,47 +1,29 @@
 var app = angular.module('scroll', ['infinite-scroll'])
 	app.controller('scrollController', ['$scope', function($scope){
 
-	$scope.today = new Date();
-	// $scope.today.setDate(28);
-	console.log($scope.today);
+	var today = new Date();
 	$scope.images = [];
 	$scope.counter = 0;
-	$scope.comment = '';
+	$scope.comment = 'Appointment';
 
 	$scope.loadMore = function(){
 
-		var last = $scope.images[$scope.images.length ];
     	for(var i = 0; i <3; i++) {
+    		today.setDate(today.getDate()+1);
+    		var dd = today.getDate();
+    		var mm = today.getMonth()+1;
+    		var y = today.getFullYear();
+    		var formattedDate = mm + '/' + dd + '/' + y;
     		var date = {
     			count: $scope.counter++,
-    			day: $scope.today++,
+    			day: formattedDate,
     			agenda: $scope.comment,
 			}
       		$scope.images.push(date);
-      		// $scope.today.setDate(27 + $scope.counter);
    		}
 	}
-	// $scope.loadMore();
+	$scope.addAgenda = function(){
+		$scope.inputarea = !$scope.inputarea
+	}
 
-	// $scope.time = function(){
-	// 	$scope.today++;
-	// 	return $scope.today
-
-	// }
 	}]);
-
-
-// var myApp = angular.module('myApp', ['infinite-scroll']);
-
-// var numbersController = function($scope){
-//     $scope.numbers = [];
-//     $scope.counter = 0;
-
-//     $scope.loadMore = function () {
-//         for (var i = 0; i < 25; i++) {
-//             $scope.numbers.push(++$scope.counter);
-//         };
-//     }
-//     $scope.loadMore();
-// }
-// myApp.controller('numbersController', numbersController); 
