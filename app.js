368e4776065f2d4b746e71496b309c7e6167082c
Student <mattray.mbr@gmail.com>
2016-01-28 15:48:46 -0700

    added dynamic button changes

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var app = angular.module('scroll', ['infinite-scroll'])
-	app.controller('scrollController', ['$scope', function($scope){
+	app.controller('scrollController', ['$scope','$timeout', function($scope,$timeout){
 
 	var today = new Date();
 	$scope.images = [];
 	$scope.counter = 0;
-	$scope.comment = '';
-	$scope.button = 'Add Appointment'
+	
 
 	$scope.loadMore = function(){
 
@@ -14,22 +13,31 @@ var app = angular.module('scroll', ['infinite-scroll'])
     		var dd = today.getDate();
     		var mm = today.getMonth()+1;
     		var y = today.getFullYear();
-    		var formattedDate = mm + '/' + dd + '/' + y;
+    		var formattedDate = mm+'/'+dd+'/'+y;
     		var date = {
     			count: $scope.counter++,
     			day: formattedDate,
     			agenda: $scope.comment,
     			visibility: false,
     			agendaVisibility: true,
-    			button: $scope.button,
+    			showEdit: false,
+    			button: 'Add Appointment',
 			} 
       		$scope.images.push(date);
    		}
 	}
-	$scope.addAgenda = function(index){
-		$scope.images[index].visibility = !$scope.images[index].visibility
-		$scope.images[index].agendaVisibility = false;
-		$scope.button = 'Save Appointment';
+	$scope.addAgenda = function(index, event){
+		$scope.images[index].visibility = true;
+		$timeout(function() {
+					event.target.nextElementSibling.focus();
+				});
 	}
+	$scope.exitText = function(index){
+		$scope.images[index].visibility = false;
+		$scope.images[index].agendaVisibility = false;
+		$scope.images[index].showEdit = true;
+		$scope.images[index].button = "Edit Appointment";
+	}
+	
 
 	}]);
